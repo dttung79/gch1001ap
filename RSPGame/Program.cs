ac493e60769d06d1c79cf421b8726c3d3ee1ce4b
dttung79 <dttung@gmail.com>
2022-11-09 16:13:57 +0700

    first commit

---
+++ RSPGame/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace RSPGame
+{
+    class Program
+    {
+        const int REWARD = 10;
+        static void Main(string[] args)
+        {
+            bool playing = true;
+            int balance = EnterBalance();
+            while (playing)
+            {
+                string computerChoice = GetComputerChoice();
+                // User chooses rock, paper, or scissors from the console
+                string userChoice = GetUserChoice();
+                Console.WriteLine("Computer chose " + computerChoice);
+                
+                // Compare the two choices and determine the winner
+                string result = CompareResults(userChoice, computerChoice);
+                balance = UpdateBalance(balance, result);
+                
+                PrintGameStatus(result, balance);
+                
+                playing = IsEnoughToPlay(balance) && IsContinue();
+            }
+            PrintFinalStatus(balance);
+        }
+        static void PrintFinalStatus(int balance)
+        {
+            Console.WriteLine("Your final balance is " + balance);
+            Console.WriteLine("Thanks for playing!");
+        }
+        static bool IsContinue() 
+        {
+            Console.Write("Do you want to play again? (y/n): ");
+            string playAgain = Console.ReadLine();
+            bool playing = playAgain == "y";
+            
+            return playing;
+        }
+        static bool IsEnoughToPlay(int balance)
+        {
+            return balance >= REWARD;
+        }
+
+        static void PrintGameStatus(string result, int balance)
+        {
+            Console.WriteLine("Game result: " + result);
+            Console.WriteLine("Your balance is now " + balance);
+        }
+        static int EnterBalance()
+        {
+            Console.Write("Enter your balance: ");
+            int balance = int.Parse(Console.ReadLine());
+            return balance;
+        }
+        static int UpdateBalance(int balance, string result)
+        {
+            if (result == "Win") balance += REWARD;
+            else if (result == "Lose") balance -= REWARD;
+            return balance;
+        }
+        static string GetUserChoice()
+        {
+            string userChoice = "";
+            bool validChoice = false;
+            while (!validChoice)
+            {
+                Console.Write("Choose Rock, Paper, or Scissors: ");
+                userChoice = Console.ReadLine();
+                validChoice = userChoice == "Rock" || userChoice == "Paper" || userChoice == "Scissors";
+                if (!validChoice)
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+            }
+            return userChoice;
+        }
+
+        static string CompareResults(string userChoice, string computerChoice)
+        {
+            if (userChoice == computerChoice)
+            {
+                return "Tie";
+            }
+            else if ((userChoice == "Rock" && computerChoice == "Scissors") ||
+                (userChoice == "Paper" && computerChoice == "Rock") ||
+                (userChoice == "Scissors" && computerChoice == "Paper"))
+            {
+                return "Win";
+            }
+            else
+            {
+                return "Lose";
+            }
+        }
+        static string GetComputerChoice()
+        {
+            // Computer chooses a random number between 1 and 3
+            Random random = new Random();
+            //int computerChoice = random.Next(1, 4);
+            string[] choices = { "Rock", "Paper", "Scissors" };
+            // convert to string (rock, paper, scissors)
+            string computerChoice = choices[random.Next(1, 4) - 1];
+            return computerChoice;
+        }
+    }
+}
---
+++ TicketSelling/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace TicketSelling
+{
+    class Program
+    {
+        static string[] movies = {"Black Adam", "Black Pather 2", "Detective Conan"};
+        const int PRICE = 5;
+        const double VIP_REDUCE = 0.1;
+        static void Main(string[] args)
+        {
+            // Hiển thị 1 menu cho người dùng chọn 3 bộ phim
+            PrintMovies(movies);
+            // Người dùng chọn phim
+            string movie = movies[GetMovie()];
+            // Người dùng chọn số lượng vé
+            int nTickets = GetNumOfTickets();
+            // Người dùng chọn ghế
+            string seats = GetSeats(nTickets);
+            // Hỏi có phải VIP member không?
+            bool VIP = IsVIPMember();
+            // In ra vé và hoá đơn
+            PrintTickets(movie, nTickets, seats);
+            PrintInvoice(nTickets, VIP);
+        }
+        static void PrintMovies(string[] movies)
+        {
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine((i+1) + ". " + movies[i]);
+            }
+        }
+        static int GetMovie()
+        {
+            Console.Write("Your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            return choice - 1;
+        }
+
+        static int GetNumOfTickets()
+        {
+            Console.Write("How many tickets? ");
+            int nTickets = int.Parse(Console.ReadLine());
+            return nTickets;
+        }
+
+        static string GetSeats(int nTickets)
+        {
+            string seats = "";
+            // use for loop to repeat nTickets time, asking a seat then add to a string
+            for (int i = 0; i < nTickets; i++)
+            {
+                Console.Write("Choose your seat: ");
+                string seat = Console.ReadLine();
+                seats += seat + " ";           
+            }
+            return seats;
+        }
+
+        static bool IsVIPMember()
+        {
+            Console.Write("Are you a VIP member? (y/n): ");
+            string answer = Console.ReadLine();
+            return answer == "y";
+        }
+
+        static void PrintTickets(string movie, int nTickets, string seats)
+        {
+            Console.WriteLine("Movie: " + movie);
+            Console.WriteLine("Number of tickets: " + nTickets);
+            Console.WriteLine("Seats: " + seats);
+        }
+
+        static void PrintInvoice(int nTickets, bool VIP)
+        {
+            double payment = nTickets * PRICE;
+            if (VIP) payment -= payment * VIP_REDUCE;
+
+            Console.WriteLine("Payment: " + payment);
+        }
+    }
+}
