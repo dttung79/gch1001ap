accfd741adaeee05c35ddf566f70a4c5e0aca8e0
dttung79 <dttung@gmail.com>
2022-11-24 08:46:33 +0700

    add topustudent

--- OOP advance/StudentManagement/TopupStudent.cs
+++ OOP advance/StudentManagement/TopupStudent.cs
@@ -5,8 +5,55 @@ using System.Threading.Tasks;
 
 namespace StudentManagement
 {
-    public class TopupStudent
+    public class TopupStudent : Student
     {
-        
+        // attributes:
+        // - grade: int (0-100)
+        // - ojt : string (Pass/Fail)
+        private int grade;
+        private string ojt;
+
+        // properties:
+        // + Grade
+        // + OJT
+        public int Grade
+        {
+            get { return grade; }
+            set
+            {
+                if (value < 0 || value > 100) value = 0;
+                grade = value;
+            }
+        }
+        public string OJT
+        {
+            get { return ojt; }
+            set 
+            {
+                if (value != "Pass" && value != "Fail") value = "Fail";
+                ojt = value;
+            }
+        }
+
+        // default constructor
+        public TopupStudent()
+        {
+            grade = 0;
+            ojt = "Fail";
+        }
+        // parameter constructor
+        public TopupStudent(string name, int age) : base(name, age)
+        {
+            grade = 0;
+            ojt = "Fail";
+        }
+
+        // ShowInfo
+        public void ShowInfo()
+        {
+            base.ShowInfo();
+            System.Console.WriteLine("Grade: " + grade);
+            System.Console.WriteLine("OJT: " + ojt);
+        }
     }
 }
