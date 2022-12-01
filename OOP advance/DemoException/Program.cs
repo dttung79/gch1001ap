8d54a9cc1b80fd8651b835092c43a5b1d4f1eb6e
dttung79 <dttung@gmail.com>
2022-12-01 11:25:05 +0700

    Add demo exceptions

---
+++ OOP advance/DemoException/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// try
+// {
+//     System.Console.WriteLine("Enter a: ");
+//     int a = int.Parse(Console.ReadLine());
+//     System.Console.WriteLine("Enter b: ");
+//     int b = int.Parse(Console.ReadLine());
+//     int c = a / b;
+//     System.Console.WriteLine("Result: " + c);
+// } 
+// catch (FormatException)
+// {
+//     System.Console.WriteLine("Invalid input");
+// }
+// catch (DivideByZeroException)
+// {
+//     System.Console.WriteLine("Divide by zero");
+// }
+try 
+{
+    Console.Write("Enter length of array: ");
+    int n = int.Parse(Console.ReadLine());
+
+    int[] a = new int[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Enter a[{0}]: ", i);
+        a[i] = int.Parse(Console.ReadLine());
+        sum += a[i];
+    }
+    Console.Write("Enter lower: "); int lower = int.Parse(Console.ReadLine());
+    Console.Write("Enter upper: "); int upper = int.Parse(Console.ReadLine());
+    int partialSum = 0;
+    for (int i = lower; i <= upper; i++)
+    {
+        partialSum += a[i];
+    }
+
+    double rate = (double) partialSum / sum;
+    System.Console.WriteLine("Rate: " + rate);
+} 
+catch (FormatException)
+{
+    System.Console.WriteLine("Invalid input");
+}
+catch (IndexOutOfRangeException)
+{
+    System.Console.WriteLine("Index out of range");
+}
+catch (DivideByZeroException)
+{
+    System.Console.WriteLine("Divide by zero");
+}
---
+++ OOP advance/DemoException2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DemoException2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = ReadInt("Enter a: ");
+            int b = ReadInt("Enter b: ");
+            int c = a / b;
+            System.Console.WriteLine("Result: " + c);
+        }
+
+        public static int ReadInt(string prompt)
+        {
+            bool invalid = true;
+            int n = 0;
+            while (invalid)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    n = int.Parse(Console.ReadLine());
+                    invalid = false;
+                }
+                catch (FormatException)
+                {
+                    System.Console.WriteLine("Invalid input. Try again.");
+                }
+            } 
+            return n;    
+        }
+    }
+}
